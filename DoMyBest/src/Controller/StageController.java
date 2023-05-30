a844825c20feaf7115859a7ba521273eacce58ac
smhrd <smhrd@DESKTOP-F1HCEAC>
2023-05-30 14:25:24 +0900

    230530 / 14:25 / 최수환 / MemberDTO 작성

--- DoMyBest/src/Controller/StageController.java
+++ DoMyBest/src/Controller/StageController.java
@@ -1,5 +1,32 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import DAO.PhoneDTO;
+
 public class StageController {
+
+	public void stageStart() {
+		
+	
+		
+		
+	
+
+		ArrayList<Integer> answerList = new ArrayList<>();
+		long start = System.currentTimeMillis();
+		long end1 = start + 60 * 1000;
+		while (System.currentTimeMillis() < end1) {
+			long end2 = start + 10 * 1000;
+			while (System.currentTimeMillis() < end2) {
+				System.out.println("주문");
+				
+				
+				
+			}
+
+		}
+
+	}
 
 }
