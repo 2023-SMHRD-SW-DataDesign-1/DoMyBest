25fe31e3a929d559d750bf2bbd0ad57672fa37ac
smhrd <smhrd@DESKTOP-TF5QIQ4>
2023-05-30 15:40:33 +0900

    230530 / 15:40 / 류상지 / view 로그인

--- DoMyBest/src/View/View.java
+++ DoMyBest/src/View/View.java
@@ -3,6 +3,8 @@ package View;
 import java.util.Scanner;
 
 import Controller.StageController;
+import Model.MemberDAO;
+import Model.MemberDTO;
 
 public class View {
 
@@ -12,8 +14,6 @@ public class View {
 		Scanner scan = new Scanner(System.in);
 		// Controller 의 메소드를 호출하기위해 Stagecontroller 객체 호출
 		StageController con = new StageController();
-
-		
 
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠉⠉⠉⠉⠀⠉⠉⠉⠉⠛⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
@@ -29,21 +29,51 @@ public class View {
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
-		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
-		System.out.println(".########..##.....##.########...######...########.########.");
-	    System.out.println(".##.....##.##.....##.##.....##.##....##..##.......##.....##");
-		System.out.println(".##.....##.##.....##.##.....##.##........##.......##.....##");
-		System.out.println(".########..##.....##.########..##...####.######...########.");
-		System.out.println(".##.....##.##.....##.##...##...##....##..##.......##...##..");
-		System.out.println(".##.....##.##.....##.##....##..##....##..##.......##....##.");
-		System.out.println(".########...#######..##.....##..######...########.##.....##");
+		System.out.println(".########..##⣿⣿⣿⣿##.########...######...########.########.");
+		System.out.println(".##⣿⣿⣿⣿##.##⣿⣿⣿⣿##.##⣿⣿⣿⣿⣿##.##⣿⣿⣿⣿##..##⣿⣿⣿⣿⣿.##⣿⣿⣿⣿##");
+		System.out.println(".##⣿⣿⣿⣿##.##⣿⣿⣿⣿##.##⣿⣿⣿⣿⣿##.##⣿⣿⣿⣿⣿⣿..##⣿⣿⣿⣿⣿..##⣿⣿⣿⣿##");
+		System.out.println(".########.##⣿⣿⣿⣿##.########..##⣿⣿#####.#######..########.");
+		System.out.println(".##⣿⣿⣿⣿##.##⣿⣿⣿⣿##.##⣿⣿⣿##...##⣿⣿⣿⣿##..##⣿⣿⣿⣿⣿..##⣿⣿⣿⣿##..");
+		System.out.println(".##⣿⣿⣿⣿##.##⣿⣿⣿⣿##.##⣿⣿⣿⣿##..##⣿⣿⣿⣿##..##⣿⣿⣿⣿⣿..##⣿⣿⣿⣿##.");
+		System.out.println(".########.#######..##⣿⣿⣿⣿⣿##..######...########.##⣿⣿⣿⣿⣿##");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
 		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
-		
-		
-		
+		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿WELCOME TO JAVA HAMBURGER⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ARE YOU READY?⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+		System.out.println("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿");
+		while (true) {
+			System.out.println("===============JAVA HAMBURER GAME 회원 시스템===============");
+			System.out.print("[1] 회원 가입 [2] 로그인 [3] 랭킹조회 [4] 종료하기 >>");
+			int num = scan.nextInt();
+			// 회원가입
+			if (num == 1) {
+				System.out.println("=== 회원가입 ===");
+				System.out.print("ID를 입력 >> ");
+				String id = scan.next();
+				System.out.print("PW를 입력 >>");
+				String pw = scan.next();
+				System.out.print("NAME를 입력 >>");
+				String name = scan.next();
+
+				MemberDAO insertCon = new MemberDAO();
+
+				insertCon.insertMember(new MemberDTO(id, pw, name));
+
+				// 정지
+			} else if (num == 2) {
+
+			} else if (num == 3) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else {
+				System.out.println("숫자를 잘못 입력하셨습니다.");
+			}
+		}
 	}
+
 }
