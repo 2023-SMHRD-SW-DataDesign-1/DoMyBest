5233bfc006496da719cecfcc7cfa71e8ce7e9a9f
smhrd <smhrd@DESKTOP-H9Q4MMC>
2023-05-30 14:33:44 +0900

    230530 / 14:33 / 선동욱 / customerDTO작성

---
+++ DoMyBest/src/Model/CustomerDTO.java
@@ -0,0 +1,54 @@
+package Model;
+
+public class CustomerDTO {
+	
+	private String name;
+	private String gender;
+	private String hamburger;
+	private String recipe;
+	
+	public CustomerDTO(String name, String gender, String hamburger, String recipe) {
+
+		this.name = name;
+		this.gender = gender;
+		this.hamburger = hamburger;
+		this.recipe = recipe;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getHamburger() {
+		return hamburger;
+	}
+
+	public void setHamburger(String hamburger) {
+		this.hamburger = hamburger;
+	}
+
+	public String getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+	
+	
+	
+	
+
+}
