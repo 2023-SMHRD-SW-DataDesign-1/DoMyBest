04575c03e2f260163faac93a3d36af5c3377c97c
smhrd <smhrd@DESKTOP-F1HCEAC>
2023-05-31 16:57:08 +0900

    230531 / 16:56 / 최수환 / ButtonController 인터페이스 꾸밈

--- DoMyBest/src/Controller/ButtonController.java
+++ DoMyBest/src/Controller/ButtonController.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 
 import Model.CustomerDAO;
 import Model.MemberDAO;
+import javazoom.jl.player.MP3Player;
 
 public class ButtonController {
 	Random ran = new Random();
@@ -19,17 +20,28 @@ public class ButtonController {
 	Connection conn;
 	PreparedStatement psmt;
 	ResultSet rs = null;
-
-
+	MP3Player mp3 = new MP3Player();
 
 	public void stageStart() { // 스테이지 시작메소드 ( 60초 )
+
+		mp3.play("Music/bgm.mp3");
 
 		long startTime = System.currentTimeMillis();
 		long endTime = startTime + TimeUnit.SECONDS.toMillis(60);
 		int count = 0;
 		while (System.currentTimeMillis() < endTime) {
 			count++;
-			System.out.println(count + " 번 손님");
+			System.out.println("¸¸♬·¯·♩¸¸♪·¯·♫¸¸¸♬·¯·♩¸¸♪·¯·♫¸¸¸¸¸♬··¯·♩¸¸♪·¯·♫¸¸¸¸¸♬");
+			System.out.println("　　　　　　 ∧＿∧");
+			System.out.println("　　　　　　（｡･ω･｡)つ━☆☆*");
+			System.out.println("　　　　　　⊂　　 ノ 　　　☆☆");
+			System.out.println("　　　　　　　し-Ｊ　　　°。+ * 。");
+			System.out.println("　　　　　　　　　　　　　　" + count + "번째 손님 등장! ｡ﾟ");
+			System.out.println("　　　　　　　　　　　　　　　　　ﾟ･｡･ﾟ");
+			System.out.println("¸¸♬·¯·♩¸¸♪·¯·♫¸¸¸♬·¯·♩¸¸♪·¯·♫¸¸¸¸¸♬··¯·♩¸¸♪·¯·♫¸¸¸¸¸♬");
+			System.out.println();
+			System.out.println("                   ♫ 띵 동 ♪  ");
+			mp3.play("Music/bell.mp3");
 			solveP();
 
 		}
@@ -46,25 +58,30 @@ public class ButtonController {
 			int temp = ran.nextInt(cdao.cList().size());
 			char recipeList[] = cdao.cList().get(temp).getRecipe().toCharArray(); // DB에 있는 recipe컬럼 값을 문자형 배열로 생성
 			System.out.println(cdao.cList().get(temp).getHamburger() + "주세요");
-
+			System.out.println(cdao.cList().get(temp).getName() + " 님의 주문입니다.");
+			System.out.println("        " + "•────[ 주문 목록 ] " + cdao.cList().get(temp).getHamburger() + "────•");
 			System.out.println();
+			System.out.println(" ⭑:༅｡.｡༅::✼✿ ──────현재 재료 목록────── ✿✼:*ﾟ:༅｡.｡༅:*･ﾟﾟ･⭑" + "\n");
 
 			for (int j = 0; j < questionList.length; j++) {
 				System.out.print("[" + (j) + "]" + questionList[j] + " ");
 			}
 			System.out.println();
-			System.out.print("레시피 : ");
+			System.out.println();
+			System.out.println("         ╔═══ 자, 그럼 햄버거를 한 번 만들어 볼까요?═══╗");
+			System.out.print("☆⁺˚*♡⁺˚*☆ ☽⋅─────•[ 햄버거 제조 순서 ]≫≫ ");
 
-			for (int j = 0; j < recipeList.length; j++) { // question 배열과 answerL배열 값을 비교해 레시피 작성
-				for (int k = 0; k < questionList.length; k++) {
-					if (recipeList[j] == (char) (k + '0')) {
-						System.out.print(questionList[k] + " ");
-					}
-				}
-			}
 			System.out.println();
 			long remainingTime = endTime - System.currentTimeMillis();
 			for (int j = 0; j < recipeList.length; j++) {
+				for (int k = 0; k < recipeList.length; k++) { // question 배열과 answerL배열 값을 비교해 레시피 작성
+					for (int l = 0; l < questionList.length; l++) {
+						if (recipeList[k] == (char) (l + '0')) {
+							System.out.print(questionList[l] + " ");
+						}
+					}
+				}
+				System.out.println("         재료 번호를 순서대로 클릭 해 주세요");
 				Button bt = new Button();
 				if (remainingTime > 0) {
 					synchronized (bt) {
@@ -78,7 +95,7 @@ public class ButtonController {
 					break;
 				}
 				if (recipeList[j] == bt.answerGet()) {
-					System.out.println("정답");
+					System.out.println("          ʚ(*´꒳`*)ɞ ~♡ 정답! 다음 재료번호 입력해주세요!" + "");
 					if (cdao.cList().get(temp).getDifficult().equals("EASY")) {
 						scon.easyLine(j); // 햄버거 쌓이는 시각효과 메서드
 					} else if (cdao.cList().get(temp).getDifficult().equals("NORMAL")) {
@@ -88,11 +105,13 @@ public class ButtonController {
 					}
 
 				} else {
-					System.out.println("오답");
+					System.out.println("        ŏ̥̥̥̥םŏ̥̥̥̥ 잘못된 재료번호입니다. 손님이 화가나서 돌아갔어요 ŏ̥̥̥̥םŏ̥̥̥̥ ");
+					System.out.println();
+					System.out.println();
 					break;
 				}
 				if (System.currentTimeMillis() > endTime) { // 지정한 시간을 넘어서 답을 쓴경우 타임아웃
-					System.out.println("타임아웃");
+					System.out.println("시간이 초과했어요 ŏ̥̥̥̥םŏ̥̥̥̥  손님이 집으로 돌아갔어요ŏ̥̥̥̥םŏ̥̥̥̥  ");
 					break;
 				}
 
